0f43c7c8e0b16c9a9db6cba75cbcf06c24f5bba3
ClowFsh <naholloway@yahoo.com>
2026-02-15 12:49:14 -0500

    the rest

---
+++ battle_song.gd
@@ -0,0 +1,10 @@
+@tool class_name BattleSong extends Resource
+
+const HIT_MARKER_NAME = "HIT"
+
+@export_file("*.ogg") var song_path: String
+@export_file("*.txt") var marker_text: String
+
+@export var markers : Array[float] = []
+
+var song_name : StringName
---
+++ rhythm_manager.gd
@@ -0,0 +1,24 @@
+extends Node
+
+@onready var audio_player: AudioStreamPlayer2D = %AudioStreamPlayer2D
+@onready var songs_
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	var t = audio_player.get_playback_position()
+	var delay = AudioServer.get_time_since_last_mix()
+	var latency = AudioServer.get_output_latency()
+	
+	var corrected_time = t + delay - latency
+
+func get_song(song_name) -> 
+
+func parse_logic_markers(file:Resource) -> Array[float]:
+	return []
+
+
+	
+	
+	
